1b1a98f83d3ca9085c14b18f99f78822bda0861b
killavulkan <killavulkan@hotmail.de>
2013-04-23 16:11:11 +0200

    update

--- src/scripts/Custom/premiumtele.cpp
+++ src/scripts/Custom/premiumtele.cpp
@@ -4,8 +4,9 @@
 
 #include "GossipDef.h"
 #include "Player.h"
-#include "precompiled.h"
-#include "ScriptMgr.h"
+#include "ScriptPCH.h"
+#include "ScriptedGossip.h"
+#include "Database/DatabaseEnv.h"
 #include "SpellMgr.h"
 #include "Common.h"
 
@@ -18,7 +19,7 @@ bool GossipHello_vip_teleporter(Player* player, Creature* creature)
 	// player->ADD_GOSSIP_ITEM(5, "Normaler Teleporter",	     GOSSIP_SENDER_MAIN, 2);
 	player->ADD_GOSSIP_ITEM(5, "Build",                         GOSSIP_SENDER_MAIN, 100);
 
-	player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,            _Creature->GetObjectGuid());
+	player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,            creature->GetGUID());
 
 	return true;	   
 }
@@ -35,7 +36,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
                 player->ADD_GOSSIP_ITEM(3, "Waffen F\303\244higkeit auf Max",                 GOSSIP_SENDER_MAIN, 6);
  			    player->ADD_GOSSIP_ITEM(0, "Sch\303\266nen Tag noch", GOSSIP_SENDER_MAIN, 101);
 
-			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,       _Creature->GetObjectGuid());
+			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,       creature->GetGUID());
 		   }	   
 		   else
 		   {
@@ -51,7 +52,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
                 player->ADD_GOSSIP_ITEM(5, "Teleport",                GOSSIP_SENDER_MAIN, 4);
  			    player->ADD_GOSSIP_ITEM(0, "Sch\303\266nen Tag noch", GOSSIP_SENDER_MAIN, 101);
 			    
-                player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,       _Creature->GetObjectGuid());
+                player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,       creature->GetGUID());
             }	   
 		   else
 		   {
@@ -67,7 +68,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
                 player->ADD_GOSSIP_ITEM(5, "Welt",                 GOSSIP_SENDER_MAIN, 102);
  			    player->ADD_GOSSIP_ITEM(0, "Zur\303\274ck",   GOSSIP_SENDER_MAIN, 0);
 
-			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,         _Creature->GetObjectGuid());
+			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,         creature->GetGUID());
 		   }	   
 		   else
 		   {
@@ -88,7 +89,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 	    	    player->ADD_GOSSIP_ITEM(5, "Quel'Danas",                  GOSSIP_SENDER_MAIN, 20);
 			    player->ADD_GOSSIP_ITEM(0, "Teleport Men\303\274",        GOSSIP_SENDER_MAIN,  2);
 
-			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          _Creature->GetObjectGuid());
+			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          creature->GetGUID());
 		   }
 		   else
 		   if (player->GetTeam() == HORDE)
@@ -102,7 +103,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 	    	    player->ADD_GOSSIP_ITEM(5, "Quel'Danas",                  GOSSIP_SENDER_MAIN, 20);
 			    player->ADD_GOSSIP_ITEM(0, "Teleport Men\303\274",        GOSSIP_SENDER_MAIN,  2);
 
-			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,            _Creature->GetObjectGuid());
+			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,            creature->GetGUID());
 		   }
 			  break;
 
@@ -113,7 +114,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 			   player->ADD_GOSSIP_ITEM(5, "Sturmwind",		               GOSSIP_SENDER_MAIN, 11);
  			   player->ADD_GOSSIP_ITEM(0, "Sch\303\266nen Tag noch",      GOSSIP_SENDER_MAIN, 101);
 
-			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,           _Creature->GetObjectGuid());
+			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,           creature->GetGUID());
 
 		   }
 		   else
@@ -122,7 +123,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 			   player->ADD_GOSSIP_ITEM(5, "Orgrimmar",                   GOSSIP_SENDER_MAIN, 16);
  			   player->ADD_GOSSIP_ITEM(0, "Sch\303\266nen Tag noch",     GOSSIP_SENDER_MAIN, 101);
 
-			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          _Creature->GetObjectGuid());
+			   player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          creature->GetGUID());
 		   }
 			  break;
 	   
@@ -141,7 +142,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 			    // player->ADD_GOSSIP_ITEM(0, "Teleport Men\303\274",             GOSSIP_SENDER_MAIN, 0);
                 player->ADD_GOSSIP_ITEM(0, "Teleport Men\303\274",        GOSSIP_SENDER_MAIN,  2);
 
-			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          _Creature->GetObjectGuid());
+			    player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,          creature->GetGUID());
 		   }
 			    break;
 
@@ -158,37 +159,37 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 #############################################
 */
         //Gurubashi Arena!
-        case  10: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,   -13240.937500f,    204.797928f,     30.869392f, 1.102942f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  10: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,   -13240.937500f,    204.797928f,     30.869392f, 1.102942f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
          //Sturmwind
-        case  11: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,    -8881.000000f,    575.000000f,     93.000000f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  11: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,    -8881.000000f,    575.000000f,     93.000000f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Darnassus
-        case  12: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,     9951.890000f,   2280.550000f,   1341.390000f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  12: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,     9951.890000f,   2280.550000f,   1341.390000f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Eisenschmiede
-        case  13: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,    -4981.000000f,   -881.000000f,    501.660000f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  13: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,    -4981.000000f,   -881.000000f,    501.660000f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Exodar
-        case  14: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,  -4014.000000f, -11895.790000f,     -2.000000f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  14: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,  -4014.000000f, -11895.790000f,     -2.000000f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Shattrath
-        case  15: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,  -1887.510010f,   5359.379883f,    -12.427300f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  15: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,  -1887.510010f,   5359.379883f,    -12.427300f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Orgrimmar
-        case  16: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,     1629.777344f,  -4373.210449f,     31.246769f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  16: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,     1629.777344f,  -4373.210449f,     31.246769f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Donnerfels
-        case  17: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,    -1196.858398f,     30.147331f,    176.949188f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  17: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,    -1196.858398f,     30.147331f,    176.949188f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
         // Unterstadt
-        case  18: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,     1844.765625f,    237.655991f,     62.275150f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  18: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(0,     1844.765625f,    237.655991f,     62.275150f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
 		// Silbermod
-        case  19: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   9400.486328f,  -7278.376953f,     14.206780f, 0.000000f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  19: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   9400.486328f,  -7278.376953f,     14.206780f, 0.000000f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
 		//Quel'Danas
-        case  20: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   12942.400391f,  -6930.770020f,     4.213060f, 2.437920f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+        case  20: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   12942.400391f,  -6930.770020f,     4.213060f, 2.437920f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
 
 /*
@@ -199,31 +200,31 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 #############################################
 */
         // Der Schwarzer Tempel
-		case  21: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   -3640.432861f,  314.126221f,     35.256004f, 2.897555f);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		case  21: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   -3640.432861f,  314.126221f,     35.256004f, 2.897555f);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //FDS: Das Auge
-		case  22: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   3088.391357f,  1386.200073f,     184.957825f, 4.546514f);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		case  22: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   3088.391357f,  1386.200073f,     184.957825f, 4.546514f);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Gruuls Unterschlupf
-		// case  23: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  23: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Höhle des Schlangenschreins
-		// case  24: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  24: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Hyjal
-		case  25: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,   -8168.119629f,  -4177.029785f,     -165.076645f, 4.125618f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		case  25: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(1,   -8168.119629f,  -4177.029785f,     -165.076645f, 4.125618f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Karazahn
-		// case  26: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  26: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Magtheridons Kammer
-		// case  27: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  27: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Sonnenbrunnenplateau
-		// case  28: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  28: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         //Zul'Aman
-		case  29: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   6851.297852f,  -7954.612793f,     170.040161f, 4.668588f); break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		case  29: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(530,   6851.297852f,  -7954.612793f,     170.040161f, 4.668588f); break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
 /*
 #############################################
@@ -233,23 +234,23 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 #############################################
 */
         //XXXXXX
-		// case  30: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  30: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
         //XXXXXX
-		// case  31: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  31: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
         //XXXXXX
-		// case  32: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  32: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 
         //XXXXXX
-		// case  33: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // _Creature->MonsterSay(4444405, player->GetObjectGuid()); break;
+		// case  33: player->CLOSE_GOSSIP_MENU(); player->TeleportTo(MAP,   XXXXXf,  YYYYf,     ZZZZZf, OOOOOf);  break; // creature->MonsterSay(4444405, player->GetGuid()); break;
 		
         case 100:
 	        player->ADD_GOSSIP_ITEM(6, "Version",                   GOSSIP_SENDER_MAIN, 110);
 	        player->ADD_GOSSIP_ITEM(6, "Entwickler",                GOSSIP_SENDER_MAIN, 111);
 	        player->ADD_GOSSIP_ITEM(0, "Sch\303\266nen Tag noch",   GOSSIP_SENDER_MAIN, 101);
 
-	        player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,        _Creature->GetObjectGuid());
+	        player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE,        creature->GetGUID());
 		break;
 
 		case 101:
@@ -265,7 +266,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 		case 110:
 			player->CLOSE_GOSSIP_MENU();
 		    player->GetSession()->SendNotification("Version 2.0.2");
-		  //_Creature->MonsterSay(4444406, player->GetObjectGuid());
+		  //creature->MonsterSay(4444406, player->GetGuid());
 	    break;
 
 		case 111:
@@ -278,7 +279,7 @@ void SendDefaultMenu_vip_teleporter(Player* player, Creature* creature, uint32 a
 bool GossipSelect_vip_teleporter(Player* player, Creature* creature, uint32 sender, uint32 action)
 {
     if (sender == GOSSIP_SENDER_MAIN)
-        SendDefaultMenu_vip_teleporter(pPlayer, _Creature, action);
+        SendDefaultMenu_vip_teleporter(player, creature, action);
 
     return true;
 }
